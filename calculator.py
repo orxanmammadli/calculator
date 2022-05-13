de29c01c36f29ecb95a738a95abf03fbc87e9722
Orkhan Mammadli <63901366+orxanmammadli@users.noreply.github.com>
2022-05-13 11:22:19 +0200

    calculator code logic added

---
+++ calculator.py
@@ -0,0 +1,40 @@
+
+from replit import clear
+from art import logo
+def add(n1,n2):
+  return n1+n2
+def subtract(n1,n2):
+  return n1-n2  
+def multiply(n1,n2):
+  return n1*n2  
+def divide(n1,n2):
+  return n1/n2
+operations={
+  "+": add, 
+  "-": subtract, 
+  "*": multiply, 
+  "/": divide
+} 
+def calculation():
+  print(logo)
+  num1=float(input("What is the first number?: "))
+  for symbol in operations:
+    print(symbol)
+  cont_this=True
+  while cont_this:
+    
+    operation_symbol=input("Pick an operation : ")  
+    num2=float(input("What is the next number?: "))    
+    calculation_function=operations[operation_symbol] 
+    answer=calculation_function(num1,num2)
+    
+    print(f"{num1}{operation_symbol}{num2}={answer}")
+  
+    ask = input("Do you want to contunue or do new calculation? y or n: ")
+    if ask=="y":
+      num1=answer
+    else:
+      cont_with = False
+      clear()
+      calculation()
+calculation()    
